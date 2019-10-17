5c18785dc534581e3f33c5d9d37b88403a7e9588
mingjun97 <470890875@qq.com>
2019-10-17 16:08:17 -0700

    Add clang_getToken

--- src/lib.rs
+++ src/lib.rs
@@ -2059,4 +2059,5 @@ link! {
     pub fn clang_TextComment_getText(comment: CXComment) -> CXString;
     pub fn clang_VerbatimBlockLineComment_getText(comment: CXComment) -> CXString;
     pub fn clang_VerbatimLineComment_getText(comment: CXComment) -> CXString;
+    pub fn clang_getToken(TU: CXTranslationUnit, Location: CXSourceLocation) -> * mut CXToken;
 }
